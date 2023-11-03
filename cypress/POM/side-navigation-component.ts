51104f7b9381eec104a9a7b0093341c6da915c2b
markomilenkovic546 <markomilenkovic546@gmail.com>
2023-11-03 15:28:40 +0100

    created SideNavigation class

--- cypress/POM/side-navigation-component.ts
+++ cypress/POM/side-navigation-component.ts
@@ -1,5 +1,45 @@
 class SideNavigation {
+//=====================Elements===========================//
+    elements: {
+        homeButton: () => Cypress.Chainable<JQuery<HTMLElement>>;
+        boardsButton: () => Cypress.Chainable<JQuery<HTMLElement>>;
+        templatesButton: () => Cypress.Chainable<JQuery<HTMLElement>>;
+        settingsButton: () => Cypress.Chainable<JQuery<HTMLElement>>;
+    }
 
+    actions: {
+        clickOnHomeButton: () => void;
+        clickOnBoardsButton: () => void;
+        clickOnTemplatesButton: () => void;
+        clickOnSettingsButton: () => void;
+
+    }
+
+
+
+    constructor() {
+        this.elements = {
+            homeButton: () => cy.get('button:contains("Home")'),
+            boardsButton: () => cy.get('button:contains("Boards")'),
+            templatesButton: () => cy.get('button:contains("Templates")'),
+            settingsButton: () => cy.get('button:contains("Settings")'),
+
+      }
+    
+        this.actions = {
+            clickOnHomeButton: () => this.elements.homeButton().click(),
+            clickOnBoardsButton: () => this.elements.boardsButton().click(),
+            clickOnTemplatesButton: () => this.elements.templatesButton().click(),
+            clickOnSettingsButton: () => this.elements.settingsButton().click()
+        }
 }
 
+//=====================Actions===========================//
+
+
+
+
+
+
+}
 export default SideNavigation
