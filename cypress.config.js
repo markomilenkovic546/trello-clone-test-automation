4b050581b5e7ce8403688eb5aa7c91f1ef5000b1
markomilenkovic546 <markomilenkovic546@gmail.com>
2023-12-26 13:42:14 +0100

    configured mochawesome reporter

--- cypress.config.js
+++ cypress.config.js
@@ -3,10 +3,12 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   viewportHeight: 1080,
   viewportWidth: 1920,
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     chromeWebSecurity: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
     baseUrl: "https://trello-clone-one.vercel.app/",
     retries: 1,
